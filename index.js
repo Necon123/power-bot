84c71f8bc2cf72fdf0de478734c9fd5187e4006a
DEVSMSM <86024087+DEVSMSM@users.noreply.github.com>
2021-08-15 19:09:34 +0300

    Update index.js

--- index.js
+++ index.js
@@ -413,5 +413,20 @@ client.on("message", async message => {
     }
 });
 
-
+client.on("message", message => {
+  if (message.content.toLowerCase() === prefix + "roles") {
+    let roles = message.guild.roles.cache.map(r => `> ${r.name}  `).join("\n");
+    let embed = new Discord.MessageEmbed()
+      .setTitle("Server Roles")
+      .setDescription(" ```javascript\n" + roles + "``` ");
+    message.channel.send(embed);
+  }
+  if (message.content.toLowerCase() === prefix + "help roles") {
+    let roles = new Discord.MessageEmbed()
+      .setTitle(`Command: roles `)
+      .addField("Usage", `${prefix}roles`)
+      .addField("Information", "Show All Roles For Server");
+    message.channel.send(roles);
+  }
+});
 
