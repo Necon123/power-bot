077ef97d2c8ebc680ebec1f80087c8044d54122b
DEVSMSM <86024087+DEVSMSM@users.noreply.github.com>
2021-08-15 19:06:52 +0300

    Update index.js

--- index.js
+++ index.js
@@ -413,22 +413,5 @@ client.on("message", async message => {
     }
 });
 
-client.on('message', message => {
-     if(!message.channel.guild) return;
- 
-                if(message.content.startsWith(prefix + 'bots')) {
- 
- 
-    if (message.author.bot) return;
-    let i = 1;
-        const botssize = message.guild.members.cache.filter(m=>m.user.bot).map(m=>`${i++} - <@${m.id}>`);
-          const embed = new Discord.MessageEmbed()
-          .setAuthor(message.author.tag, message.author.avatarURL())
-          .setDescription(`**Found ${message.guild.members.cache.filter(m=>m.user.bot).size} bots in this Server**
-${botssize.join('\n')}`)
-.setFooter(client.user.username, client.user.avatarURL())
-.setTimestamp();
-message.channel.send(embed)
- 
-}})
+
 
