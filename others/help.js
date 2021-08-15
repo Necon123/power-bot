4064ecd3ad3f71b783dc5cb60c54868696cd0577
Necon123 <86918032+Necon123@users.noreply.github.com>
2021-08-15 21:15:30 +0100

    Update help.js

--- others/help.js
+++ others/help.js
@@ -39,7 +39,7 @@ module.exports = {
 
 ⚒️|**Moderation Commands**
 > \`${PREFIX}lock\`   **-**  \`${PREFIX}unlock\`  
-> \`${PREFIX}ban\`    **-**  \`${PREFIX}ban\`    
+> \`${PREFIX}unban\`    **-**  \`${PREFIX}ban\`    
 > \`${PREFIX}slowmode\`
 
 <a:link:864142531758260234>|**Links**
